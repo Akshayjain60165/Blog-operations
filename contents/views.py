0229a70343b32a5ee788f515c3a2ab01d250caec
Akshay Jain <69035152+Akshayjain60165@users.noreply.github.com>
2021-05-20 18:18:54 +0530

    Update views.py

--- contents/views.py
+++ contents/views.py
@@ -1,6 +1,6 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from random import randint
 # Create your views here.
 from .models import blog,blog_comments
 
@@ -10,10 +10,13 @@ def homepage(request):
 
 def addblog(request):
    if request.method == 'POST':
-        id = int(request.POST['id'])
         place = request.POST['place']
         title  =  request.POST['title']
-        p = blog(blog_id = id , place = place, title = title)
+        is_unique = True
+        while is_unique == True: 
+            pk = randint(1, 1999) # 19 digits: 1, random 18 digits
+            is_unique = blog.objects.filter(id=pk).exists()
+        p = blog(blog_id = pk , place = place, title = title)
         p.save()
         return HttpResponse('done')
         
